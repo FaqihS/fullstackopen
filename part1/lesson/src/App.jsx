a0bbb1d75b9b4512518c29db19eb13e59ffc5424
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2023-08-23 11:09:36 +0800

    part: 1a

---
+++ part1/lesson/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+const Hello = (props) => {
+  console.log(props)
+  return (
+  <>
+   <p>Hello {props.name}{prop.age ? `, you are ${props.age} old`: ""}</p>
+  </>
+  )
+}
+
+const App = () => {
+  const name = "Rommy"
+  const age = 10
+  return (
+  <div>
+    <h1>Greetings</h1>
+    <Hello name={name}/>
+    <Hello name="Dennis" age={age + 1}/>
+  </div>
+  )
+}
+
+export default App
