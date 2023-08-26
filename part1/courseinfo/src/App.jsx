7f4a0a428e5a8ac71e758d99fcc9b02745115f33
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2023-08-26 16:38:59 +0800

    Revert "part : 2"
    This reverts commit 57dca190647ceb016deeee5966e81c1210b55aaa.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,54 +1,52 @@
-import Course from "./componets/Course"
+  const Header = props =>  <h1>{props.course.name}</h1>
+
+
+
+  const Part = props =>  <p>{props.name} {props.exercise}</p>
+  const Content = (props) => {
+    return<>{
+    props.course.parts.map(part =>{
+      return <Part name={part.name} exercise={part.exercises} />
+    })
+  }</>
+  }
+
+  const Total = (props) =>{
+    let total = 0
+
+    props.course.parts.forEach(part => {
+      total += part.exercises
+    })
+
+
+    return <p>Number of Exercises {total} </p>
+  } 
+
 const App = () => {
 
-const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
-      {courses.map(course => <Course course={course} key={course.id}/>)}
-      
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total  course={course}/>
     </div>
   )
 }
