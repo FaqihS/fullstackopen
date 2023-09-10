6b71fa46942b00312e76608307aba5873616444d
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2023-09-10 10:44:08 +0800

    part:4b lesson:notes

--- part4/lesson/controller/note.js
+++ part4/lesson/controller/note.js
@@ -1,51 +1,61 @@
-const notesRouter = require('express').Router()
-const Note = require('../models/note')
+const notesRouter = require("express").Router();
+const Note = require("../models/note");
 
-notesRouter.get('/',(req, res)=>{
-  Note.find({}).then(notes => {
-    res.json(notes)
-  })
-})
+notesRouter.get("/", async (req, res) => {
+  const notes = await Note.find({});
+  res.json(notes);
+});
 
-notesRouter.get('/:id',(req,res,next)=>{
-  Note.findById(req.params.id).then(note=>{
+notesRouter.get("/:id", async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id);
     note ? res.json(note) : res.status(404).end()
-  }).catch(err=>next(err))
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
-notesRouter.post('/',(req,res,next)=>{
-  const body = req.body
+notesRouter.post("/", async (req, res, next) => {
+  const body = req.body;
 
   const note = new Note({
     content: body.content,
-    important: body.important
-  })
+    important: body.important || false,
+  });
 
-  note.save().then(savedNote => {
-    res.json(savedNote)
-  }).catch(err => next(err))
-})
+  try {
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
+  } catch (error) {
+    next(error);
+  }
+});
 
-notesRouter.delete('/:id',(req,res,next)=>{
-  Note.findByIdAndDelete(req.params.id).then(result =>{
-    res.status(204).end()
-  }).catch(err=>next(err))
-})
+notesRouter.delete("/:id", async (req, res, next) => {
+  try {
+    await Note.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    next(err);
+  }
+});
 
-notesRouter.put('/:id',(req,res,next)=>{
-  const body = req.body
+notesRouter.put("/:id", async (req, res, next) => {
+  const body = req.body;
 
   const note = {
     content: body.content,
-    important: body.important
+    important: body.important,
+  };
+
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, {
+      new: true,
+    });
+    res.json(updatedNote);
+  } catch (error) {
+    next(error);
   }
+});
 
-  Note.findByIdAndUpdate(req.params.id,note, { new: true })
-  .then(updatedNote=>{
-      res.json(updatedNote)
-    })
-  .catch(err => next(err))
-})
-
-module.exports =  notesRouter
-
+module.exports = notesRouter;
---
+++ part4/lesson/tests/note_api.test.js
@@ -0,0 +1,120 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const Note = require("../models/note");
+const {initialNotes,nonExistingId,notesInDb} = require('./test_helper.js')
+
+const api = supertest(app);
+
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
+
+test("notes are returned as json", async () => {
+  await api
+    .get("/api/notes")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+
+test("all notes returned", async () => {
+  const response = await api.get("/api/notes");
+  expect(response.body).toHaveLength(initialNotes.length);
+});
+
+test("the specific note is in the returned note", async () => {
+  const response = await api.get("/api/notes");
+  const contents = response.body.map((r) => r.content);
+  expect(contents).toContain("Browser can execute only JavaScript");
+});
+
+test("a valid note can be added", async () => {
+  const newNote = {
+    content: "async/await simplifies making async calls",
+    important: true,
+  };
+
+  await api
+    .post("/api/notes")
+    .send(newNote)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const notesAtEnd = await notesInDb()
+
+  expect(notesAtEnd).toHaveLength(initialNotes.length + 1);
+  const content = notesAtEnd.map(n => n.content)
+
+  expect(content).toContain("async/await simplifies making async calls");
+});
+
+test('note with no content not added',async () => {
+  const newNote = {
+    important: true,
+  }
+  await api
+  .post('/api/notes')
+  .send(newNote)
+  .expect(400)
+
+  const notesAtEnd = await notesInDb()
+
+  expect(notesAtEnd).toHaveLength(initialNotes.length)
+})
+
+test('specific note can be viewed',async () => {
+  const notesAtStart = await notesInDb()
+
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api.get(`/api/notes/${noteToView.id}`)
+  .expect(200)
+  .expect('Content-Type',/application\/json/)
+
+  expect(resultNote.body).toEqual(noteToView)
+})
+
+test('a note can be deleted',async () =>{
+  const notesAtStart = await notesInDb()
+
+  const noteToDelete = notesAtStart[0]
+
+  await api.delete(`/api/notes/${noteToDelete.id}`)
+  .expect(204)
+
+  const noteAtEnd = await notesInDb()
+
+  expect(noteAtEnd).toHaveLength(initialNotes.length -1)
+
+  const content = noteAtEnd.map(n=>n.content)
+  expect(content).not.toContain(noteToDelete.content)
+})
+
+test('note can be updated', async () => {
+  const notesAtStart = await notesInDb()
+  const noteToUpdate = notesAtStart[0]
+  const newNote={
+    content: 'newnote'
+  }
+
+  const updatedNote = await api.put(`/api/notes/${noteToUpdate.id}`)
+  .send(newNote)
+  .expect(200)
+  .expect('Content-Type',/application\/json/)
+
+  expect(updatedNote.body).not.toEqual(noteToUpdate)
+
+  const noteAtEnd = await notesInDb()
+  expect(noteAtEnd).toHaveLength(initialNotes.length)
+})
+
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
